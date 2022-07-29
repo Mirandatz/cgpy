3d486338f541e20babd8d9d51b0ec1d12396b5cc
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-07-29 20:13:13 -0300

    Add colors.py

---
+++ cgpy/colors.py
@@ -0,0 +1,16 @@
+import dataclasses
+
+MIN_COLOR_INTENSITY = 0.0
+MAX_COLOR_INTENSITY = 1.0
+
+
+@dataclasses.dataclass(frozen=True, slots=True)
+class Color:
+    red: float
+    green: float
+    blue: float
+
+    def __post_init__(self) -> None:
+        assert MIN_COLOR_INTENSITY <= self.red <= MAX_COLOR_INTENSITY
+        assert MIN_COLOR_INTENSITY <= self.green <= MAX_COLOR_INTENSITY
+        assert MIN_COLOR_INTENSITY <= self.blue <= MAX_COLOR_INTENSITY
