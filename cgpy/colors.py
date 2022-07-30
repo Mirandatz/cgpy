b49d3e7435e39402694b2a573e430048d4e529f5
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-07-29 23:40:46 -0300

    Add new method to Color

--- cgpy/colors.py
+++ cgpy/colors.py
@@ -2,6 +2,7 @@ import dataclasses
 
 MIN_COLOR_INTENSITY = 0.0
 MAX_COLOR_INTENSITY = 1.0
+MAX_CHANNEL_VALUE = 255
 
 
 @dataclasses.dataclass(frozen=True, slots=True)
@@ -14,3 +15,9 @@ class Color:
         assert MIN_COLOR_INTENSITY <= self.red <= MAX_COLOR_INTENSITY
         assert MIN_COLOR_INTENSITY <= self.green <= MAX_COLOR_INTENSITY
         assert MIN_COLOR_INTENSITY <= self.blue <= MAX_COLOR_INTENSITY
+
+    def as_rgb_ints(self) -> tuple[int, int, int]:
+        r = int(self.red * MAX_CHANNEL_VALUE)
+        g = int(self.green * MAX_CHANNEL_VALUE)
+        b = int(self.blue * MAX_CHANNEL_VALUE)
+        return r, g, b
