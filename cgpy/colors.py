a5dc16fd7a6b8b9247b02d0348f7282f70d6d4df
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2022-07-30 15:48:30 -0300

    Simplify colors.py

--- cgpy/colors.py
+++ cgpy/colors.py
@@ -1,11 +1,8 @@
 import dataclasses
-import typing
 
 MIN_COLOR_INTENSITY = 0.0
 MAX_COLOR_INTENSITY = 1.0
 MAX_CHANNEL_VALUE = 255
-
-ColorId = typing.NewType('ColorId', int)
 
 
 @dataclasses.dataclass(frozen=True, slots=True)
@@ -18,9 +15,3 @@ class Color:
         assert MIN_COLOR_INTENSITY <= self.red <= MAX_COLOR_INTENSITY
         assert MIN_COLOR_INTENSITY <= self.green <= MAX_COLOR_INTENSITY
         assert MIN_COLOR_INTENSITY <= self.blue <= MAX_COLOR_INTENSITY
-
-    def as_rgb_ints(self) -> tuple[int, int, int]:
-        r = int(self.red * MAX_CHANNEL_VALUE)
-        g = int(self.green * MAX_CHANNEL_VALUE)
-        b = int(self.blue * MAX_CHANNEL_VALUE)
-        return r, g, b
